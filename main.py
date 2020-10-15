6fd52d91ca0410f7935ccfff281409d43784dd39
Vira <chawlav@asbindia.org>
2020-10-15 19:58:04 +0530

    Create main.py
    My project is a guessing game. The computer generates a random number and the player has to guess what the number is. Each time the player guesses, the computer will tell the player if their guess is right or wrong.

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+
+n = random.randint(1, 10)
+count = 0   # this is used to tell the player how many times it took them to guess the correct number
+introduction = ('Hi there', 'Welcome', 'Hey')
+greeting = random.choice(introduction)
+
+name = raw_input(greeting + ', What is your name?' + ":")
+print()
+
+print(greeting + " " + name + "! " + 'Welcome to Guess A Random Number.')
+print()
+print('In this game, I will generate a random number between 1-10 and you have to guess what number I chose. Enjoy!!')
+print()
+print('I am now choosing a number between 1-10.')
+print()
+
+while True:
+    guesses = input('Go on, guess a number between 1-10!: ')
+    guesses = int(guesses)  # this makes the player input into an integer as initally whatever the player inputs is a string
+
+    if guesses == n:
+        print('Good job! You guessed correctly!')
+        break
+
+    elif guesses < n:
+        print('Oops! You guessed too low. Try again!')
+        count += 1
+
+    else:
+        print('Oops! You guessed too high. Try again!')
+        count += 1
+
+print('It took you' , count, 'tries! Not bad')
